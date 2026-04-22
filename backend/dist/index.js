7c6c68eac2806e5c2fe1ed973c413a3747835799
Mahidul Islam <mahidulislam3397@gmail.com>
2026-04-22 15:59:56 +0600

    openRouter Connected

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,6 +1,45 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
+import 'dotenv/config';
 const key = process.env.OPEN_ROUTER_KEY;
-console.log(key);
+import OpenAI from 'openai';
+const client = new OpenAI({
+    baseURL: 'https://openrouter.ai/api/v1',
+    apiKey: process.env.OPEN_ROUTER_KEY,
+});
+// First API call with reasoning
+async function main() {
+    const apiResponse = await client.chat.completions.create({
+        model: 'arcee-ai/trinity-large-preview:free',
+        messages: [
+            {
+                role: 'user',
+                content: "how are you",
+            },
+        ],
+        // reasoning { enabled: true }
+    });
+    const response = apiResponse.choices[0]?.message.content;
+    console.log(`The user response is -`, response);
+    // Preserve the assistant message with reasoning_details
+    // const messages = [
+    //     {
+    //         role: 'user' as const,
+    //         content: "How many r's are in the word 'strawberry'?",
+    //     },
+    //     {
+    //         role: 'assistant' as const,
+    //         content: response.content,
+    //         reasoning_details: response.reasoning_details, // Pass back unmodified
+    //     },
+    //     {
+    //         role: 'user' as const,
+    //         content: "Are you sure? Think carefully.",
+    //     },
+    // ];
+    // // Second API call - model continues reasoning from where it left off
+    // const response2 = await client.chat.completions.create({
+    //     model: 'minimax/minimax-m2.5:free',
+    //     messages, // Includes preserved reasoning_details
+    // });
+}
+main();
 //# sourceMappingURL=index.js.map
